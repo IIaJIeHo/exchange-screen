7f12350ce4a5d789845dd47d6a470357ab052756
Igor Dolzhenkov <idolzhenkov@btes.ru>
2017-02-25 18:30:59 +0300

    Refactor App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,8 +7,6 @@ import Currency from '../Currency/Currency';
 import Header from '../Header/Header';
 import './App.css';
 
-/* make should component update in all components*/
-/* optimise code in all componenent */
 /* make style better of the code */
 /* es-lint suggestion */
 
@@ -26,9 +24,6 @@ class App extends Component {
       notEnough: true
     }
 
-    console.log('this.props.rates');
-    console.log(this.props.current);
-
     this.onExchange = this.onExchange.bind(this);
     this.setCurrentCurrency = this.setCurrentCurrency.bind(this);
     this.onInputChange = this.onInputChange.bind(this);
@@ -38,16 +33,9 @@ class App extends Component {
   }
 
   notEnough(activeChange,pocket,currency){
-    console.log('not Enough');
-    console.log(activeChange);
-    console.log(currency);
-    console.log(activeChange[currency]);
-    console.log(pocket);
     if ((!activeChange[currency])||(activeChange[currency] <= 0 )||(activeChange[currency] > pocket[currency])){
-      console.log('notEnough: true');
       this.setState({notEnough: true});
     } else {
-      console.log('notEnough: false');
       this.setState({notEnough: false});
     }
   }
@@ -62,53 +50,43 @@ class App extends Component {
   }
 
   onCancel(e){
-    e.preventDefault();
     let base = {};
     base[this.state.current] = '';
     this.setState({activeChange: Object.assign({},this.state.activeChange,base)});
   }
 
   onExchange(e){
-    console.log(this.props.actions);
-    e.preventDefault();
-    let pocket = this.props.pocket;
-    
-    let active = this.state.activeChange;
-    let current = this.state.current;
-    let next = this.state.next; /* Сделать разборку */
-    let base = {};
+    let pocket = this.props.pocket,
+        active = this.state.activeChange,
+        current = this.state.current,
+        next = this.state.next,
+        base = {};
+
     base[current] = Math.round((pocket[current] - active[current])*100)/100;
     base[next] = Math.round((pocket[next] + active[current]*this.props.rates[next]/this.props.rates[current])*100)/100;
     pocket = Object.assign({},pocket,base);
-    console.log('new  pocket!!!!');
-    console.log(pocket);
+
     this.notEnough(this.state.activeChange,pocket,this.state.current);
     this.props.actions.updatePocket(pocket);
   }
 
   setCurrentCurrency(currency,type) {
+    let rates, nextIndex, newValue;
     type == "start" ? this.setState({current: currency}) : this.setState({next: currency});
     if (type == "start") {
       this.notEnough(this.state.activeChange,this.props.pocket,currency);
       if (currency ==  this.state.next) {
-        let rates = Object.keys(this.props.rates);
-        let nextIndex = rates.indexOf(this.state.next);
-        let newNext = rates[nextIndex - 1];
-        console.log('newNext');
-        console.log(newNext);
-        this.setState({next: newNext});
-        console.log('next next next');
-        
+        rates = Object.keys(this.props.rates);
+        nextIndex = rates.indexOf(this.state.next);
+        newValue = rates[nextIndex - 1];
+        this.setState({next: newValue});
       }
     } else {
       if (currency ==  this.state.current) {
-        let rates = Object.keys(this.props.rates);
-        let nextIndex = rates.indexOf(this.state.current);
-        let newCurrent = rates[nextIndex - 1];
-        console.log('newCurrent');
-        console.log(newCurrent);
-        this.setState({current: newCurrent});
-        console.log('current current current');
+        rates = Object.keys(this.props.rates);
+        nextIndex = rates.indexOf(this.state.current);
+        newValue = rates[nextIndex - 1]; /* make a small function for this transform */
+        this.setState({current: newValue});
       }
     }
 
@@ -117,25 +95,40 @@ class App extends Component {
 
   render() {
 
-    var settings = {
-      dots: true
-    };
-
     return (
       <div className="App">
-        <Header onCancel={this.onCancel} notEnough={this.state.notEnough} onExchange={this.onExchange} rates={this.props.rates} current={this.state.current} next={this.state.next}/>
-        <Currency {...this.state} notEnough={this.state.notEnough} activeChange={this.state.activeChange} rates={this.props.rates} pocket={this.props.pocket} type="start" onInputChange={this.onInputChange} setCurrentCurrency={this.setCurrentCurrency} />
-        <Currency {...this.state} activeChange={this.state.activeChange} rates={this.props.rates} pocket={this.props.pocket} type="end" onInputChange={this.onInputChange} setCurrentCurrency={this.setCurrentCurrency}/>
+        <Header onCancel={this.onCancel} 
+                notEnough={this.state.notEnough} 
+                onExchange={this.onExchange} 
+                rates={this.props.rates} 
+                current={this.state.current} 
+                next={this.state.next}/>
+        <Currency {...this.state} 
+                  notEnough={this.state.notEnough} 
+                  activeChange={this.state.activeChange} 
+                  rates={this.props.rates} 
+                  pocket={this.props.pocket} 
+                  type="start" 
+                  onInputChange={this.onInputChange} 
+                  setCurrentCurrency={this.setCurrentCurrency} />
+        <Currency {...this.state} 
+                  activeChange={this.state.activeChange} 
+                  rates={this.props.rates} 
+                  pocket={this.props.pocket} 
+                  type="end" 
+                  onInputChange={this.onInputChange} 
+                  setCurrentCurrency={this.setCurrentCurrency} />
       </div>
     );
   }
 }
 
+App.propTypes = {
+  rates: PropTypes.object.isRequired,
+  pocket: PropTypes.object.isRequired
+};
 
 function mapStateToProps(state, ownProps) {
-  console.log('mapStateToProps');
-  console.log(state);
-
   let availableCurrencies = ['USD','RUB','EUR'],
       rates = {};
 
